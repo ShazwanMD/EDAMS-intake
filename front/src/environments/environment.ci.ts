4ca96475e0baf272f7fe7a519773b5c7c9e1978c
Administrator <fauzan@umk.edu.my>
2018-10-09 11:09:44 +0800

    update environments

--- front/src/environments/environment.ci.ts
+++ front/src/environments/environment.ci.ts
@@ -1,8 +1,8 @@
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 export const environment: { production: boolean, endpoint: string, imports: any[] } = {
   production: true,
-  // endpoint: 'http://localhost:8080',
-  endpoint: 'http://edams.umk.edu.my:8080',
+  endpoint: 'http://localhost:8080',
+  // endpoint: 'http://edams.umk.edu.my:8080',
   imports: [
     StoreDevtoolsModule.instrumentOnlyWithExtension({maxAge: 5}),
   ],
