bb89b0df5dfc53f21a28b6e172e43a3015c3021e
Administrator <fauzan@umk.edu.my>
2018-11-25 14:07:08 +0800

    update user comments and upload new user manual for applicant

--- back/src/main/java/my/edu/umk/pams/intake/application/service/ApplicationSchedulerImpl.java
+++ back/src/main/java/my/edu/umk/pams/intake/application/service/ApplicationSchedulerImpl.java
@@ -24,62 +24,68 @@ import my.edu.umk.pams.intake.system.model.InEmailQueueStatus;
 import my.edu.umk.pams.intake.system.service.SystemService;
 
 @Component("applicationScheduler")
-public class ApplicationSchedulerImpl implements ApplicationScheduler{
-	
+public class ApplicationSchedulerImpl implements ApplicationScheduler {
+
 	private static final Logger LOG = LoggerFactory.getLogger(ApplicationSchedulerImpl.class);
-	
-    @Autowired
-    private PolicyService policyService;
-    
-    @Autowired
-    private ApplicationService applicationService;
-    
-    @Autowired
-    private SystemService systemService;
+
+	@Autowired
+	private PolicyService policyService;
+
+	@Autowired
+	private ApplicationService applicationService;
+
+	@Autowired
+	private SystemService systemService;
 
 	@Scheduled(cron = "*/10 * * * * *")
 	public void sendEmail() {
 		this.reminderApplicant();
 	}
-	
+
 	public void reminderApplicant() {
-		// TODO Auto-generated method stub
-		InIntakeSession intakeSession = policyService.findCurrentIntakeSession();
-		List<InIntake> intake = policyService.findIntakes(intakeSession);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Date todayDate = new Date();
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(todayDate);
-		cal.add(Calendar.DATE, 7); // 7 is the days you want to add or subtract
-		todayDate = cal.getTime();
-		String warnDate = sdf.format(todayDate);
-
-		for (InIntake inIntake : intake) {
-
-			String endDate = sdf.format(inIntake.getEndDate());
-			if (warnDate.equals(endDate)) {
-				List<InIntakeApplication> draftApplication = applicationService.findIntakeApplications(inIntake,
-						InBidStatus.DRAFTED);
-				LOG.debug("{} reminder email to be sent", draftApplication.size());
-				for (InIntakeApplication inIntakeApplication : draftApplication) {
-					boolean emailQueueExist = systemService.hasEmailQueue(inIntakeApplication.getEmail());
-					if (emailQueueExist == true) {
-						InEmailQueue email = new InEmailQueueImpl();
-						String subject = "Reminder";
-						String body = "Please complete your application. You have 7 days to go.. Hurry up times is running up";
-						// verification.getToken();
-						email.setTo(inIntakeApplication.getEmail());
-						email.setSubject(subject);
-						email.setBody(body);
-						// method send email
-						email.setCode("EQ/" + System.currentTimeMillis());
-						email.setQueueStatus(InEmailQueueStatus.QUEUED);
-						systemService.saveEmailQueue(email);
-					}
-				}
-			}
-		}
-
+		// // TODO Auto-generated method stub
+		// InIntakeSession intakeSession =
+		// policyService.findCurrentIntakeSession();
+		// List<InIntake> intake = policyService.findIntakes(intakeSession);
+		// SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		// Date todayDate = new Date();
+		// Calendar cal = Calendar.getInstance();
+		// cal.setTime(todayDate);
+		// cal.add(Calendar.DATE, 7); // 7 is the days you want to add or
+		// subtract
+		// todayDate = cal.getTime();
+		// String warnDate = sdf.format(todayDate);
+		//
+		// for (InIntake inIntake : intake) {
+		//
+		// String endDate = sdf.format(inIntake.getEndDate());
+		// if (warnDate.equals(endDate)) {
+		// List<InIntakeApplication> draftApplication =
+		// applicationService.findIntakeApplications(inIntake,
+		// InBidStatus.DRAFTED);
+		// LOG.debug("{} reminder email to be sent", draftApplication.size());
+		// for (InIntakeApplication inIntakeApplication : draftApplication) {
+		// boolean emailQueueExist =
+		// systemService.hasEmailQueue(inIntakeApplication.getEmail());
+		// if (emailQueueExist == true) {
+		// InEmailQueue email = new InEmailQueueImpl();
+		// String subject = "Reminder";
+		// String body = "Please complete your application. You have 7 days to
+		// go.. Hurry up times is running up";
+		// // verification.getToken();
+		// email.setTo(inIntakeApplication.getEmail());
+		// email.setSubject(subject);
+		// email.setBody(body);
+		// // method send email
+		// email.setCode("EQ/" + System.currentTimeMillis());
+		// email.setQueueStatus(InEmailQueueStatus.QUEUED);
+		// systemService.saveEmailQueue(email);
+		// }
+		// }
+		// }
+		// }
+		//
+		//
 	}
 
 }
